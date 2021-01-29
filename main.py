d1217d86e4ebabadd017a42c53a0c441f89baf4b
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-29 16:03:35 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True: 
-    print(input.rotation(Rotation.ROLL)) 
-    if input.rotation(Rotation.ROLL) > 0 or input.rotation(Rotation.ROLL) < 0:
+    print(input.acceleration(Dimension.X)) 
+    if input.acceleration(Dimension.X) > 0 or input.acceleration(Dimension.X) < 0:
         light.set_all(light.rgb(255, 0, 255))
     else:
         light.clear()    
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
-    console.log(input.rotation(Rotation.Roll))
-    if (input.rotation(Rotation.Roll) > 0 || input.rotation(Rotation.Roll) < 0) {
+    console.log(input.acceleration(Dimension.X))
+    if (input.acceleration(Dimension.X) > 0 || input.acceleration(Dimension.X) < 0) {
         light.setAll(light.rgb(255, 0, 255))
     } else {
         light.clear()
