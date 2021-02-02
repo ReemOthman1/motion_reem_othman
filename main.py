5c31ee27dd2af2f9cddb401df2e868130347d9d7
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-02-02 10:18:51 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ while True:
         light.set_all(light.rgb(255, 0, 255))
     if input.acceleration(Dimension.Y) > 0 or input.acceleration(Dimension.Y) < 0:
         light.set_all(light.rgb(255, 255, 0))
-    if input.acceleration(Dimension.Z) > 0 or input.acceleration(Dimension.Z) < 0:
-        light.set_all(light.rgb(0, 255,0))
     else:
         light.clear()    
         
