0191977a3034268016b6bde818430d6c12087593
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-02-02 10:16:30 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-while True: 
-    print(input.acceleration(Dimension.X)) 
+while True:  
     if input.acceleration(Dimension.X) > 0 or input.acceleration(Dimension.X) < 0:
         light.set_all(light.rgb(255, 0, 255))
+    if input.acceleration(Dimension.Y) > 0 or input.acceleration(Dimension.Y) < 0:
+        light.set_all(light.rgb(255, 0, 0))
+    if input.acceleration(Dimension.Z) > 0 or input.acceleration(Dimension.Z) < 0:
+        light.set_all(light.rgb(0, 0, 255))
     else:
         light.clear()    
+        
--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 while (true) {
-    console.log(input.acceleration(Dimension.X))
     if (input.acceleration(Dimension.X) > 0 || input.acceleration(Dimension.X) < 0) {
         light.setAll(light.rgb(255, 0, 255))
+    }
+    
+    if (input.acceleration(Dimension.Y) > 0 || input.acceleration(Dimension.Y) < 0) {
+        light.setAll(light.rgb(255, 0, 0))
+    }
+    
+    if (input.acceleration(Dimension.Z) > 0 || input.acceleration(Dimension.Z) < 0) {
+        light.setAll(light.rgb(0, 0, 255))
     } else {
         light.clear()
     }
