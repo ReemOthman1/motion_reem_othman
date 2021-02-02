564af6dcafdcac2fcaf376dbbd116e7d491c9e61
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-02-02 10:28:12 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 while True:  
     if input.acceleration(Dimension.X) > 7 or input.acceleration(Dimension.X) < 7:
-        light.set_all(light.rgb(255, 0, 255))
+        light.set_pixel_color(4,(light.rgb(255, 0, 255)))
     if input.acceleration(Dimension.Y) > 20 or input.acceleration(Dimension.Y) < 20:
-        light.set_all(light.rgb(255, 255, 0))
+        light.set_pixel_color(0,(light.rgb(255, 255, 0)))
     else:
-        light.clear()    
+        light.clear()   
         
--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 while (true) {
     if (input.acceleration(Dimension.X) > 7 || input.acceleration(Dimension.X) < 7) {
-        light.setAll(light.rgb(255, 0, 255))
+        light.setPixelColor(4, light.rgb(255, 0, 255))
     }
     
     if (input.acceleration(Dimension.Y) > 20 || input.acceleration(Dimension.Y) < 20) {
-        light.setAll(light.rgb(255, 255, 0))
+        light.setPixelColor(0, light.rgb(255, 255, 0))
     } else {
         light.clear()
     }
