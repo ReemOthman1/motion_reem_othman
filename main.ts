dbac3c2ed2ea703565381b35ea70e042545eac46
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-02-02 10:48:13 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -5,8 +5,6 @@ while (true) {
     
     if (input.acceleration(Dimension.Z) > 20 || input.acceleration(Dimension.Z) < 20) {
         light.setPixelColor(0, light.rgb(255, 255, 0))
-    } else {
-        light.clear()
     }
     
 }
