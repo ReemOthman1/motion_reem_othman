3f89a4752e173c3902f0299623ad02c4d43afe7a
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-02-03 18:33:33 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 while True:  
-    if input.acceleration(Dimension.X) > 7 or input.acceleration(Dimension.X) < 7:
+    if input.acceleration(Dimension.X) > 0 or input.acceleration(Dimension.X) < 0:
         light.set_pixel_color(9,(light.rgb(255, 0, 255)))
-    if input.acceleration(Dimension.Z) > 20 or input.acceleration(Dimension.Z) < 20:
+        music.ba_ding.play_until_done()
+    if input.acceleration(Dimension.Z) > 400 or input.acceleration(Dimension.Z) < 500:
         light.set_pixel_color(0,(light.rgb(255, 255, 0)))
+        music.wawawawaa.play_until_done()
     else:
         light.clear()
--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 while (true) {
-    if (input.acceleration(Dimension.X) > 7 || input.acceleration(Dimension.X) < 7) {
+    if (input.acceleration(Dimension.X) > 0 || input.acceleration(Dimension.X) < 0) {
         light.setPixelColor(9, light.rgb(255, 0, 255))
+        music.baDing.playUntilDone()
     }
     
-    if (input.acceleration(Dimension.Z) > 20 || input.acceleration(Dimension.Z) < 20) {
+    if (input.acceleration(Dimension.Z) > 400 || input.acceleration(Dimension.Z) < 500) {
         light.setPixelColor(0, light.rgb(255, 255, 0))
+        music.wawawawaa.playUntilDone()
     } else {
         light.clear()
     }
